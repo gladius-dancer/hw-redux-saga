f982fea9136c600b4d77afab3d79127c9826c547
Rustem <gladius.dancer@gmail.com>
2023-06-09 14:28:51 +0500

    Sixth

--- src/sagas/counterSaga.ts
+++ src/sagas/counterSaga.ts
@@ -1,9 +1,10 @@
-import {apply, call, put, takeEvery} from "redux-saga/effects";
+import {apply, call, put, delay, takeEvery} from "redux-saga/effects";
 import {ASYNC_LOAD_DATA, loadData, loadDataFail, loadDataSuccess} from "../store/dataReducer";
 
 function* asyncLoadData(): any {
     try {
-        yield put(loadData())
+        yield put(loadData());
+        yield delay(3000);
         const response = yield call(fetch, "https://5f7998dbe402340016f9321f.mockapi.io/jobs");
         const data = yield apply(response, response.json, []);
         yield put(loadDataSuccess(data));
